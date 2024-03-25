bf6df9661d09a24ab11e14c06bf0cd09297e77f0
ADI <adiheydari2@gmail.com>
2024-03-25 13:57:28 -0700

    change Headers & add icons

--- src/Component/Headers.jsx
+++ src/Component/Headers.jsx
@@ -1,21 +1,36 @@
 import {MdLocationOn} from "react-icons/md"
+import {HiCalendar, HiSearch} from "react-icons/hi"
 
 function Headers() {
   return (
     <div className="header">
         <div className="headerSearch">
             <div className="headerSearchItem">
-                <MdLocationOn className="headerIcon"/>
+                <MdLocationOn className="headerIcon locationIcon"/>
                 <input type="text"
                  placeholder="Where to go?" 
                  className="headerSearchInput" 
                  name="destination"  
                  id="destination"/>
+                 <span className="seperator"></span>
                 
             </div>
-            <div className="headerSearchItem"></div>
-            <div className="headerSearchItem"></div>
-            <div className="headerSearchItem"></div>
+            <div className="headerSearchItem">
+                <HiCalendar className="headerIcon dateIcon"/>
+                <div className="dateDropDown">2023/06/22</div>
+                <span className="seperator"></span>
+            </div>
+            <div className="headerSearchItem">
+                <div id="optionDropDown">
+                    1 adult &bull; 2 children &bull; 1 room
+                </div>
+            <span className="seperator"></span>
+            </div>
+            <div className="headerSearchItem">
+                <button className="headerSearchBtn">
+                    <HiSearch className="headerIcon"/>
+                </button>
+            </div>
 
         </div>
     </div>
