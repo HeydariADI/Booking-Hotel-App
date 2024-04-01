db9ad33b83f2d03a1d95e5e96ecf64fe84fcd21e
ADI <adiheydari2@gmail.com>
2024-04-01 13:46:23 -0700

    create useFetch hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 
+import { Toaster } from "react-hot-toast";
 import "./App.css";
 import Headers from "./Component/Headers";
 function App() {
   return (
     <div>
+      <Toaster/>
       <Headers/>
     </div>
   )
