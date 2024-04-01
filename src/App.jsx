48fd0761d9fa10efbedf4a9679740cb554fa7637
ADI <adiheydari2@gmail.com>
2024-04-01 15:14:51 -0700

    install react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import { Toaster } from "react-hot-toast";
 import "./App.css";
 import Header from "./Component/Header";
 import LocationList from "./Component/LocationList";
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div>
       <Toaster/>
       <Header/>
-      <LocationList/>
+      <Routes>
+        <Route path="/" element = {<LocationList/>}/>
+      </Routes>
+
     </div>
   )
 
