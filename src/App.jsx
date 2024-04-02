154d4bf35048c511f64f2b5084f577814843c896
ADI <adiheydari2@gmail.com>
2024-04-01 20:11:54 -0700

    AppLayout Component & nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 import Header from "./Component/Header";
 import LocationList from "./Component/LocationList";
 import { Route, Routes } from "react-router-dom";
+import AppLayout from "./Component/AppLayout";
 function App() {
   return (
     <div>
@@ -11,6 +12,11 @@ function App() {
       <Header/>
       <Routes>
         <Route path="/" element = {<LocationList/>}/>
+        <Route path="/hotels" element = {<AppLayout/>}>
+          <Route index element = {<div>hotels</div>}/>
+          <Route path=":id" element = {<div>single hotel</div>}/>
+
+        </Route>
       </Routes>
 
     </div>
