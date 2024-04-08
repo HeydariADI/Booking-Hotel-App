be1f94156c5ad94f610d99924264304432ea50ce
ADI <adiheydari2@gmail.com>
2024-04-08 11:01:20 -0700

    useGeoLocation Hook

--- src/Component/Map.jsx
+++ src/Component/Map.jsx
@@ -1,27 +1,33 @@
-<<<<<<< HEAD
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMap,
+  useMapEvent,
+} from "react-leaflet";
 import { useEffect, useState } from "react";
-import { useHotels } from "./Context/HotelsProvider"
-import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
-import { useSearchParams } from "react-router-dom";
-=======
-import { useState } from "react";
-import { useHotels } from "./Context/HotelsProvider"
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
->>>>>>> 978602c0e6f94da4b9c6ec514b378b88df37de24
+import { useNavigate, useSearchParams } from "react-router-dom";
+import useGeoLocation from "../../hooks/useGeoLocation";
+import useUrlLocation from "../../hooks/useUrlLocation";
 
+function Map({ markerLocations }) {
+  const [mapCenter, setMapCenter] = useState([20, 4]);
+  const [lat, lng] = useUrlLocation();
+  const {
+    isLoading: isLoadingPosition,
+    position: geoLocationPosition,
+    getPosition,
+  } = useGeoLocation();
 
+  useEffect(() => {
+    if (lat && lng) setMapCenter([lat, lng]);
+  }, [lat, lng]);
 
-function Map() {
-  const {isloading,hotels} = useHotels();
-<<<<<<< HEAD
-  const [mapCenter,setMapcenter] = useState([51.505, -0.09]);
-  const [searchParams,setSearchParams] = useSearchParams();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
-
-  useEffect(()=>{
-    if(lat & lng) setMapcenter([lat,lng]);
-  },[lat,lng])
+  useEffect(() => {
+    if (geoLocationPosition?.lat && geoLocationPosition?.lng)
+      setMapCenter([geoLocationPosition.lat, geoLocationPosition.lng]);
+  }, [geoLocationPosition]);
 
   return (
     <div className="mapContainer">
@@ -31,51 +37,37 @@ function Map() {
         zoom={6}
         scrollWheelZoom={true}
       >
-      <TileLayer
+        <button onClick={getPosition} className="getLocation">
+          {isLoadingPosition ? "Loading ..." : " Use Your Location"}
+        </button>
+        <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-     <ChangeCenter position={mapCenter} />
-        {hotels.map((item) => (
+        <DetectClick />
+        <ChangeCenter position={mapCenter} />
+        {markerLocations.map((item) => (
           <Marker key={item.id} position={[item.latitude, item.longitude]}>
             <Popup>{item.host_location}</Popup>
           </Marker>
         ))}
       </MapContainer>
-  
     </div>
   );
-=======
-  const [mapCenter,setMapcenter] = useState([
-    20,3
-
-  ]);
-  return (
-    <div className="mapContainer">
-      <MapContainer className="map" center={mapCenter} zoom={13} scrollWheelZoom={true}>
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    {hotels.map((item)=>(
-        <Marker key={item.id} position={[item.longitude,item.longitude] }>
-        <Popup>
-          {item.host_location}
-        </Popup>
-      </Marker>
-    ))}
-  
-  </MapContainer>
-  
-    </div>
-  )
->>>>>>> 978602c0e6f94da4b9c6ec514b378b88df37de24
 }
-
-export default Map
+export default Map;
 
 function ChangeCenter({ position }) {
   const map = useMap();
   map.setView(position);
+  return null;
+}
+
+function DetectClick() {
+  const navigate = useNavigate();
+  useMapEvent({
+    click: (e) =>
+      navigate(`/bookmark/add?lat=${e.latlng.lat}&lng=${e.latlng.lng}`),
+  });
   return null;
 }
